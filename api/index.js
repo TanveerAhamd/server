fb0ad10bb7f0983e50c07741281738ded8a0ccba
Tanveer Ahmad <119504718+TanveerAhamd@users.noreply.github.com>
2023-12-14 16:42:59 +0500

    liver mongoos account

--- api/index.js
+++ api/index.js
@@ -325,7 +325,7 @@ app.get('/api/messages/:complainid', async (req, res) => {
 
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/complains").then(() => {
+mongoose.connect("mongodb+srv://saad:DKdnKtF19CjhTI4k@cluster0.nqx7zzf.mongodb.net").then(() => {
   console.log("db  is running on port 3003 ")
   app.listen(3003, () => {
     console.log("db and server is running on port 3003 ")
