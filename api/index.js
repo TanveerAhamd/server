d2349eba01a7e5ccacfe590cd6c80c6398c63ec7
Tanveer Ahmad <119504718+TanveerAhamd@users.noreply.github.com>
2024-01-26 16:23:09 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -382,7 +382,7 @@ app.get('/api/messages/:complainid', async (req, res) => {
 // tanveer db 
 // mongodb+srv://mtanveerulhassan2:PGQaPVMYsSCJ9yRB@cluster0.krsilsu.mongodb.net/
 
-mongoose.connect("mongodb+srv://mtanveerulhassan2:PGQaPVMYsSCJ9yRB@cluster0.krsilsu.mongodb.net/TicerComplain").then(() => {
+mongoose.connect("mongodb+srv://mtanveerulhassan2:pu3E6rttg4VexM5P@cluster0.vykalzk.mongodb.net/TicerComplain").then(() => {
   console.log("db  is running on port 3003 ")
   app.listen(3003, () => {
     console.log("db and server is running on port 3003 ")
