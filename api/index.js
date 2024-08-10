6372695ede872081ce077194d08957b0801683ba
Tanveer Ahmad <119504718+TanveerAhamd@users.noreply.github.com>
2024-08-10 12:07:41 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -382,7 +382,14 @@ app.get('/api/messages/:complainid', async (req, res) => {
 // tanveer db 
 // mongodb+srv://mtanveerulhassan2:PGQaPVMYsSCJ9yRB@cluster0.krsilsu.mongodb.net/
 // mongodb+srv://mtanveerulhassan2:pu3E6rttg4VexM5P@cluster0.vykalzk.mongodb.net
+// DrBMroRj5adOVFrw   password of complaintnew on mongodb atlas
 
+// mongoose.connect("mongodb+srv://mtanveerulhassan2:pu3E6rttg4VexM5P@cluster0.vykalzk.mongodb.net/TicerComplain").then(() => {
+//   console.log("db  is running on port 3003 ")
+//   app.listen(3003, () => {
+//     console.log("db and server is running on port 3003 ")
+//   })
+// });
 mongoose.connect("mongodb+srv://mtanveerulhassan2:pu3E6rttg4VexM5P@cluster0.vykalzk.mongodb.net/TicerComplain").then(() => {
   console.log("db  is running on port 3003 ")
   app.listen(3003, () => {
